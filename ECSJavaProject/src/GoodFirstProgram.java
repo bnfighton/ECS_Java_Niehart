9e26d740ba8ffc5bf46b0b7cace4ca43f93c8f17
Ben Niehart <bnfighton@gmail.com>
2017-12-01 10:23:34 -0800

    Variables done

---
+++ ECSJavaProject/src/GoodFirstProgram.java
@@ -0,0 +1,16 @@
+
+public class GoodFirstProgram 
+{
+	
+public static void main(String[] args)
+{
+	System.out.println("Hello World");
+	System.out.println("Hello Again");
+	System.out.println("I Like Typing This.");
+	System.out.println("This Is Fun.");
+	System.out.println("Yay! Printing.");
+	System.out.println("I'd Much Rather You 'Not'.");
+	System.out.println("I \"Said\" Do Not Touch This.");
+
+}
+}
---
+++ ECSJavaProject/src/YourInitials.java
@@ -0,0 +1,18 @@
+
+public class YourInitials {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("B B B B B           N            N");
+		System.out.println("B         B         N N          N");
+		System.out.println("B         B         N  N         N");
+		System.out.println("B        B          N   N        N");
+		System.out.println("B B  B B            N    N       N");
+		System.out.println("B        B          N     N      N");
+		System.out.println("B          B        N      N     N");
+		System.out.println("B           B       N       N    N");
+		System.out.println("B           B       N        N   N");
+		System.out.println("B         B         N         N  N");
+		System.out.println("B B B B B           N          N N");
+	}
+}
